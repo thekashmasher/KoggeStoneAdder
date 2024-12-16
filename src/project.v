a942677110768a559ff9cc8f6a71c7f2798b655f
thekashmasher <120293801+thekashmasher@users.noreply.github.com>
2024-12-15 20:24:36 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Aaquil Kasham
+ * Copyright (c) 2024 Your Name
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_kogge_stone (
+module tt_um_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
